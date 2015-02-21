99910e1c8749c7d6405041c4eee1626bccc7e5ec
Juan Carrano <juan@carrano.com.ar>
2015-02-21 12:09:16 -0300

    Add fixed point library.

---
+++ ext_libs/fixed_point/src/vector.c
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * @author	Juan I Carrano
+ * @author	Juan I. Ubeira
+ * @copyright	Copyright 2014 by Juan I Carrano, Andrés Calcabrini, Juan I. Ubeira and
+ * 		Nicolás Venturo. All rights reserved. \n
+ * 		\n
+ *		This program is free software: you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation, either version 3 of the License, or
+ *		(at your option) any later version. \n
+ * 		\n
+ *		This program is distributed in the hope that it will be useful,
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *		GNU General Public License for more details. \n
+ * 		\n
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Vector operations.
+ */
+
+#define FXP_DECLARATION FXP_DECLARATION_C99_HEADER
+
+#include "fixed_point/fixed_point.h"
+
+#undef FXP_DECLARATION
+#define FXP_DECLARATION FXP_DECLARATION_C99_BODY
+
+#include "fixed_point/vector.h"
+
