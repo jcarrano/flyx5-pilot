a0b4cc40587c4c4660cd567de76f3d2b2574da8e
Juan Ignacio Ubeira <jubeira@outlook.com>
2015-03-24 19:15:43 -0300

    Added doxy support

--- src/quad_setup.h
+++ src/quad_setup.h
@@ -15,6 +15,14 @@
 #define ESC_SETUP_BUTTON BUTTON_1
 #define DMU_SETUP_BUTTON BUTTON_2
 
+//! Handles IMU calibration. Use inside a loop with updated statePtr from nlcf filter.
+//!
+//! @param calibrationMode Set true if user is already calibrating the device when the function is called, or false if not.
+//! @param statePtr Reference to current filter status.
+//! @return Returns true if the device's calibration process is still going on, false otherwise.
+//! @note Usage: press BTN2 to enter calibration mode, then to trigger measurement 1 & 2, and once more to accept and finish.
+//! @note Sounds will be triggered after entering calibration mode, and after second measurement to tell the user how precise the process was.
+
 bool qset_TryDmuCalibration(bool calibrationMode, struct nlcf_state* statePtr);
 
 #endif /* QUAD_SETUP_H_ */
